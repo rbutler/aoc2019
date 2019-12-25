0afb7eb7e5d441d75b39ee8965ce63186d27152f
Ryan Butler <butler.ryan@gmail.com>
2019-12-25 11:06:16 -0600

    part 1

--- src/Day03.hs
+++ src/Day03.hs
@@ -19,7 +19,7 @@ data Direction = U | D | L | R
 
 type Coord = (Int, Int)
 
-type CoordMap = Data.Map Coord Bool
+type CoordMap = Map.Map Coord Bool
 
 dirToAction :: Direction -> Coord
 dirToAction U = (0, -1)
@@ -47,11 +47,28 @@ input = (map (map processLine . splitLine) . inputRaw) "input/Day03.txt"
     processLine [] = error "No input"
     splitLine = splitOn ","
 
-addMotion :: CoordMap -> Coord -> Motion -> CoordMap
-addMotion coords current mot =
+addMotion :: CoordMap -> Coord -> Motion -> (CoordMap, Coord)
+addMotion coords currentCoord@(x,y) (Motion dir n)
+  | n == 0 = (coords, currentCoord)
+  | dir == U = addMotion (Map.insert (x,y) True coords) (move currentCoord (0, 1)) (Motion dir (n - 1))
+  | dir == D = addMotion (Map.insert (x,y) True coords) (move currentCoord (0, -1)) (Motion dir (n - 1))
+  | dir == L = addMotion (Map.insert (x,y) True coords) (move currentCoord (-1, 0)) (Motion dir (n - 1))
+  | dir == R = addMotion (Map.insert (x,y) True coords) (move currentCoord (1, 0)) (Motion dir (n - 1))
+  | otherwise = error "this broke"
+
+runMotions :: CoordMap -> Coord -> [Motion] -> CoordMap
+runMotions coords _ [] = coords
+runMotions coords currentCoord (m:ms) = runMotions cm p ms
+  where (cm, p) = addMotion coords currentCoord m
+
+--mManhattan :: CoordMap -> Int
+--mManhattan (Map.Map (x,y) _) = manhattan (x,y)
 
 run1 :: IO ()
 run1 = do
-  let map1 = foldr addMotion (Map.empty :: CoordMap) (first input)
+  let map1 = runMotions (Map.empty :: CoordMap) (0,0) (input !! 0)
+  let map2 = runMotions (Map.empty :: CoordMap) (0,0) (input !! 1)
+  let ix = Map.intersection map1 map2
+  let mans = map manhattan $ Map.keys ix
   --result <- evaluate $ input
-  print result
+  print $ show mans
